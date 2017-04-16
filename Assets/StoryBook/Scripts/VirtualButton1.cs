a21519a989d116103fd3ad2e8f7f2862e6ef7767
jae <jekm58@gmail.com>
2017-04-16 20:47:54 +0900

    리셋

--- Assets/StoryBook/Scripts/VirtualButton1.cs
+++ Assets/StoryBook/Scripts/VirtualButton1.cs
@@ -5,21 +5,21 @@ using System;
 
 public class VirtualButton1 : MonoBehaviour, IVirtualButtonEventHandler{
 
-    
-
     private GameObject resetButton;
 
     void Start () {
         resetButton = GameObject.Find("resetButton");
-        resetButton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
 
-	}
+        resetButton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        resetButton.SetActive(false);
+    }
 
 
 
 
     public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
     {
+        Debug.Log("button1");
         GameManager.manager.inactiveNext();
         //throw new NotImplementedException();
        
