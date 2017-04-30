45c4ea47b8c7a4e4353507c495afc4c4eedb67f4
Jae <kjhee940@naver.com>
2017-05-01 02:01:08 +0900

    2p에 빨간모자 이동, 늑대 이동

---
+++ Assets/(0)StoryBook/Scripts/LRRH_nav.cs
@@ -0,0 +1,152 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LRRH_nav : MonoBehaviour {
+
+    public GameObject[] LRRH_destination; //목적지
+    private bool[] des_flag;
+
+    public float speed = 10.0f;
+    public float minDistance = 0.1f;
+    private Vector3 nomaliedVector;
+
+
+    private MarkerStateManager mMarkerStateManager;
+
+    void Awake()
+    {
+        mMarkerStateManager = GameObject.Find("MarkerManager").GetComponent<MarkerStateManager>();
+        des_flag = new bool[LRRH_destination.Length];
+        for (int i = 0; i < des_flag.Length; i++)
+            des_flag[i] = false;
+        nomaliedVector = Vector3.zero - transform.position;
+    }
+
+    void Update()
+    {
+        if (mMarkerStateManager.getStoneMarker() == MarkerStateManager.StateType.On)
+            Move();
+    }
+
+
+    private void Move()
+    {
+        //1번째 목적지로
+        if(des_flag[0] == false)
+        {
+            if (Vector3.Distance(transform.position, LRRH_destination[0].transform.position) > minDistance)
+            {
+                Vector3 vDirection = LRRH_destination[0].transform.position - transform.position;
+                Vector3 vMoveVector = vDirection.normalized * speed * Time.deltaTime;
+                transform.position += vMoveVector;
+
+                Quaternion turretRotation = Quaternion.LookRotation(LRRH_destination[0].transform.position - transform.position);
+                transform.rotation = Quaternion.Slerp(transform.rotation, turretRotation, Time.deltaTime * 0.05f);
+
+                //원점기준 법선벡터
+                Vector3 nomalvector1 = (Vector3.zero - LRRH_destination[0].transform.position).normalized;
+            }
+        }
+        //마지막 목적지
+        if (des_flag[3] == true)
+        {
+            if (Vector3.Distance(transform.position, LRRH_destination[4].transform.position) > minDistance)
+            {
+                Vector3 vDirection = LRRH_destination[4].transform.position - transform.position;
+                Vector3 vMoveVector = vDirection.normalized * speed * Time.deltaTime;
+                transform.position += vMoveVector;
+
+                Quaternion turretRotation = Quaternion.LookRotation(LRRH_destination[4].transform.position - transform.position);
+                transform.rotation = Quaternion.Slerp(transform.rotation, turretRotation, Time.time * 0.05f);
+            }
+        }
+        //3번째 목적지
+        else if (des_flag[2] == true)
+        {
+            if (Vector3.Distance(transform.position, LRRH_destination[3].transform.position) > minDistance)
+            {
+                Vector3 vDirection = LRRH_destination[3].transform.position - transform.position;
+                Vector3 vMoveVector = vDirection.normalized * speed * Time.deltaTime;
+                transform.position += vMoveVector;
+
+                Quaternion turretRotation = Quaternion.LookRotation(LRRH_destination[3].transform.position - transform.position);
+                transform.rotation = Quaternion.Slerp(transform.rotation, turretRotation, Time.time * 0.05f);
+            }
+        }
+        //2번째 목적지
+        else if (des_flag[1] == true)
+        {
+            if (Vector3.Distance(transform.position, LRRH_destination[2].transform.position) > minDistance)
+            {
+                Vector3 vDirection = LRRH_destination[2].transform.position - transform.position;
+                Vector3 vMoveVector = vDirection.normalized * speed * Time.deltaTime;
+                transform.position += vMoveVector;
+
+                Quaternion turretRotation = Quaternion.LookRotation(LRRH_destination[2].transform.position - transform.position);
+                transform.rotation = Quaternion.Slerp(transform.rotation, turretRotation, Time.time * 0.05f);
+            }
+        }
+        //1번째 목적지
+        else if (des_flag[0] == true)
+        {
+            if (Vector3.Distance(transform.position, LRRH_destination[1].transform.position) > minDistance)
+            {
+                Vector3 vDirection = LRRH_destination[1].transform.position - transform.position;
+                Vector3 vMoveVector = vDirection.normalized * speed * Time.deltaTime;
+                transform.position += vMoveVector;
+
+                Quaternion turretRotation = Quaternion.LookRotation(LRRH_destination[1].transform.position - transform.position);
+                transform.rotation = Quaternion.Slerp(transform.rotation, turretRotation, Time.time * 0.05f);
+            }
+        }
+        
+        
+        
+    }
+
+
+    //충돌
+    void OnTriggerEnter(Collider coll)
+    {
+        //최종 목적지 도달.
+        if (coll.tag == "EndCube1")
+        {
+            Debug.Log("도착");
+            gameObject.SetActive(false);
+            
+        }
+
+        //중간 목적지 충돌.
+        if (coll.tag == "destination")
+        {
+            if (des_flag[0] == false)
+            {
+                des_flag[0] = true;
+                LRRH_destination[0].SetActive(false);
+            }
+            else if (des_flag[3] == true)
+            {
+                des_flag[4] = true;
+                LRRH_destination[4].SetActive(false);
+            }
+            else if (des_flag[2] == true)
+            {
+                des_flag[3] = true;
+                LRRH_destination[3].SetActive(false);
+            }
+            else if (des_flag[1] == true)
+            {
+                des_flag[2] = true;
+                LRRH_destination[2].SetActive(false);
+            }
+            else if (des_flag[0] == true)
+            {
+                des_flag[1] = true;
+                LRRH_destination[1].SetActive(false);
+            }
+        }
+
+    }
+
+
+}
