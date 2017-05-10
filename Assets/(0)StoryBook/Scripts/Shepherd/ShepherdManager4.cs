f8e137a9194ce554c8fe3229f68d7b0ee3f84992
Jae <kjhee940@naver.com>
2017-05-10 16:41:18 +0900

    4페 양치기 동선 완료

--- Assets/(0)StoryBook/Scripts/Shepherd/ShepherdManager4.cs
+++ Assets/(0)StoryBook/Scripts/Shepherd/ShepherdManager4.cs
@@ -5,6 +5,8 @@ public class ShepherdManager4 : MonoBehaviour {
 
     //목적지
     public GameObject[] destination;
+    //사람
+    public GameObject person;
     //동작 순서 체크
     private bool[] desFlag;
     //애니메이션 1번 체크
@@ -52,7 +54,7 @@ public class ShepherdManager4 : MonoBehaviour {
                 transform.position += vMoveVector;
 
                 Quaternion turretRotation = Quaternion.LookRotation(destination[0].transform.position - transform.position);
-                transform.rotation = Quaternion.Slerp(transform.rotation, turretRotation, Time.deltaTime * 0.05f);
+                transform.rotation = Quaternion.Slerp(transform.rotation, turretRotation, Time.deltaTime * 5f);
 
             }
         }
@@ -63,7 +65,49 @@ public class ShepherdManager4 : MonoBehaviour {
             desFlag[1] = !desFlag[1];
             StartCoroutine("move1");
         }
+        //마을사람들 나타난 후 목적지 1로.
+        else if (desFlag[2])
+        {           
+            if (Vector3.Distance(transform.position, destination[1].transform.position) > minDistance)
+            {
+                anim.SetBool("run", true);
+                Vector3 vDirection = destination[1].transform.position - transform.position;
+                Vector3 vMoveVector = vDirection.normalized * runSpeed * Time.deltaTime;
+                transform.position += vMoveVector;
 
+                Quaternion turretRotation = Quaternion.LookRotation(destination[1].transform.position - transform.position);
+                transform.rotation = Quaternion.Slerp(transform.rotation, turretRotation, Time.deltaTime * 5f);
+            }
+        }
+        //마을사람들을 향해 돈다.
+        else if (desFlag[3])
+        {
+            anim.SetBool("run", false);
+            Quaternion turretRotation = Quaternion.LookRotation(person.transform.position - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, turretRotation, Time.deltaTime * 5f);
+            StartCoroutine("move3");
+        }
+        //늑대가 나타났다고 한다.
+        else if (desFlag[4])
+        {
+            anim.SetTrigger("talk2");
+            desFlag[4] = !desFlag[4];
+            StartCoroutine("move4");
+        }
+        //서북쪽길로 감.
+        else if (desFlag[5])
+        {
+            if (Vector3.Distance(transform.position, destination[2].transform.position) > minDistance)
+            {
+                anim.SetBool("run", true);
+                Vector3 vDirection = destination[2].transform.position - transform.position;
+                Vector3 vMoveVector = vDirection.normalized * runSpeed * Time.deltaTime;
+                transform.position += vMoveVector;
+
+                Quaternion turretRotation = Quaternion.LookRotation(destination[2].transform.position - transform.position);
+                transform.rotation = Quaternion.Slerp(transform.rotation, turretRotation, Time.deltaTime * 5f);
+            }
+        }
     }
 
     //늑대가 나타났다고 외침
@@ -71,8 +115,23 @@ public class ShepherdManager4 : MonoBehaviour {
     {
         yield return new WaitForSeconds(2.0f);
         anim.SetTrigger("talk2");
-        //yield return new WaitForSeconds(2.0f);
-        //desFlag[3] = true;
+        yield return new WaitForSeconds(2.0f);
+        anim.SetTrigger("talk2");
+        yield return new WaitForSeconds(2.0f);
+        desFlag[2] = true;
+    }
+    //딜레이
+    IEnumerator move3()
+    {
+        yield return new WaitForSeconds(2.0f);
+        desFlag[3] = false;
+        desFlag[4] = true;
+    }
+    //딜레이
+    IEnumerator move4()
+    {
+        yield return new WaitForSeconds(2.0f);
+        desFlag[5] = true;
     }
 
     //충돌
@@ -89,6 +148,13 @@ public class ShepherdManager4 : MonoBehaviour {
         //중간 목적지 충돌.
         if (coll.tag == "destination")
         {
+            //1목적지. 
+            if (desFlag[2] == true)
+            {
+                desFlag[2] = false;
+                desFlag[3] = true;
+                destination[1].SetActive(false);
+            }
             //0목적지. 
             if (desFlag[0] == true)
             {
@@ -96,8 +162,6 @@ public class ShepherdManager4 : MonoBehaviour {
                 desFlag[1] = true;
                 destination[0].SetActive(false);
             }
-
-
         }
 
     }
